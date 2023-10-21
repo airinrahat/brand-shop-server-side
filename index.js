05dbf1313a5d96405db488337e6ba4d82367a7cd
Airin Rahat Prome <airinrahatprome@gmail.com>
2023-10-21 06:17:25 -0700

    update a data

--- index.js
+++ index.js
@@ -98,12 +98,6 @@ async function run() {
     });
 
     // Detele only this user toy operation
-    app.delete("/addtocart/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await CartCollection.deleteOne(query);
-      res.send(result);
-    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
